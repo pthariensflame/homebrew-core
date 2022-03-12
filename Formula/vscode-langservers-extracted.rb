954b7ff56cafda635ea6aa9e3afcddeca5968901
Alexander Ronald Altman <alexanderaltman@me.com>
2022-03-12 12:32:18 -0800

    vscode-langservers-extracted 4.1.0 (new formula) : DRAFT

---
+++ Formula/vscode-langservers-extracted.rb
@@ -0,0 +1,21 @@
+require "language/node"
+
+class VscodeLangserversExtracted < Formula
+  desc "Language servers for HTML, CSS, JavaScript, and JSON originally extracted from VSCode"
+  homepage "https://github.com/hrsh7th/vscode-langservers-extracted"
+  url "https://github.com/hrsh7th/vscode-langservers-extracted/archive/refs/tags/v4.1.0.tar.gz"
+  sha256 "1cb1c8fdea07f70ae3602952ba06b962e6bd60ed50d09d5dce3c5ad3b914030e"
+  license "MIT"
+  head "https://github.com/hrsh7th/vscode-langservers-extracted.git", branch: "master"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "false"
+  end
+end
