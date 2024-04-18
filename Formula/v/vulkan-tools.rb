5745854a42a69b76a37f91a95fd5b1306c959855
Laine Taffin Altman <alexanderaltman@me.com>
2024-04-18 13:19:54 -0700

    vulkan-tools 1.3.287

--- Formula/v/vulkan-tools.rb
+++ Formula/v/vulkan-tools.rb
@@ -1,8 +1,8 @@
 class VulkanTools < Formula
   desc "Vulkan utilities and tools"
   homepage "https://github.com/KhronosGroup/Vulkan-Tools"
-  url "https://github.com/KhronosGroup/Vulkan-Tools/archive/refs/tags/v1.3.280.tar.gz"
-  sha256 "97cbe660c066eb5b00a2f33b501f7cd0baaf8996b997fcba185ce298c8835fed"
+  url "https://github.com/KhronosGroup/Vulkan-Tools/archive/refs/tags/v1.3.287.tar.gz"
+  sha256 "960e438cf0d77ec651d118655a2265437e6befdb628899378e04f9bfd7658df2"
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/Vulkan-Tools.git", branch: "main"
 
@@ -51,7 +51,7 @@ class VulkanTools < Formula
       inreplace buildpath.glob("*/macOS/*/CMakeLists.txt") do |s|
         s.gsub! "${MOLTENVK_DIR}/MoltenVK/include",
                 "${MOLTENVK_DIR}/include"
-        s.gsub! "${MOLTENVK_DIR}/MoltenVK/dylib/macOS/libMoltenVK.dylib",
+        s.gsub! "${MOLTENVK_DIR}/Package/Release/MoltenVK/dynamic/dylib/macOS/libMoltenVK.dylib",
                 "${MOLTENVK_DIR}/lib/libMoltenVK.dylib"
       end
     end
