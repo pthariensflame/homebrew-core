b7cbd1911a28888eca7eb87a1f08b671428c4e8d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 07:58:30 +0000

    colmap 4.2.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/colmap.rb
+++ Formula/c/colmap.rb
@@ -1,10 +1,9 @@
 class Colmap < Formula
   desc "Structure-from-Motion and Multi-View Stereo"
   homepage "https://colmap.github.io/"
-  url "https://github.com/colmap/colmap/archive/refs/tags/4.1.1.tar.gz"
-  sha256 "0cadd938756d7046055751ca35bcf0d35911403fcb65b91d022ddc418dc110a5"
+  url "https://github.com/colmap/colmap/archive/refs/tags/4.2.0.tar.gz"
+  sha256 "b61731fb1a4a33609e64fb353fe589d483be6a73878a5965b7d32ae23fb22fc5"
   license "BSD-3-Clause"
-  revision 3
 
   bottle do
     sha256 cellar: :any, arm64_tahoe:   "e1751ac1e2ef1a407d0e6a2b455b93c2e5d6514aab241b6df162bd1deb1358b1"
@@ -33,7 +32,6 @@ class Colmap < Formula
   depends_on "onnxruntime"
   depends_on "openimageio"
   depends_on "openssl@3"
-  depends_on "poselib"
   depends_on "qtbase"
   depends_on "qtsvg"
   depends_on "suite-sparse"
@@ -50,7 +48,29 @@ class Colmap < Formula
     depends_on "mesa"
   end
 
+  # TODO: Restore the poselib dependency when a release includes the required solvers.
+  # Release tracking: https://github.com/PoseLib/PoseLib/issues/180
+  # Required solver: https://github.com/PoseLib/PoseLib/pull/208
+  resource "poselib" do
+    url "https://github.com/PoseLib/PoseLib/archive/fa7280fee27f97aff31ae7f98bab7f583fac7d08.tar.gz"
+    sha256 "ff1c44342ab2c84ac9ac74c4028762c50d12ab5a483d20466cc1f635fe8f4d1f"
+  end
+
   def install
+    resource("poselib").stage do
+      system "cmake", "-S", ".", "-B", "build", "-DBUILD_SHARED_LIBS=ON",
+                      *std_cmake_args(install_prefix: libexec/"poselib")
+      system "cmake", "--build", "build"
+      system "cmake", "--install", "build"
+    end
+
+    # Keep the private PoseLib available to consumers of COLMAP's CMake package.
+    inreplace "cmake/colmap-config.cmake.in", "set(FETCH_POSELIB @FETCH_POSELIB@)",
+              <<~CMAKE.chomp
+                set(FETCH_POSELIB @FETCH_POSELIB@)
+                set(PoseLib_DIR "${PACKAGE_PREFIX_DIR}/libexec/poselib/lib/cmake/PoseLib")
+              CMAKE
+
     args = %w[
       -DCUDA_ENABLED=OFF
       -DFETCH_POSELIB=OFF
@@ -59,9 +79,11 @@ class Colmap < Formula
       -DBUILD_SHARED_LIBS=ON
     ]
 
+    args << "-DPoseLib_DIR=#{libexec}/poselib/lib/cmake/PoseLib"
+
     # Fix library install directory and rpath
     inreplace "CMakeLists.txt", "LIBRARY DESTINATION thirdparty/", "LIBRARY DESTINATION lib/"
-    args << "-DCMAKE_INSTALL_RPATH=#{loader_path}"
+    args << "-DCMAKE_INSTALL_RPATH=#{loader_path};#{loader_path}/../libexec/poselib/lib"
     # Set openssl@3 to avoid indirect linkage with openssl@4
     # TODO: switch to openssl@4
     args << "-DOPENSSL_ROOT_DIR=#{formula_opt_prefix("openssl@3")}"
