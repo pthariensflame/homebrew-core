aaf4f6a8874cfccff10121f1984c04c5a2867344
botantony <antonsm21@gmail.com>
2026-09-07 23:53:02 +0200

    moor 2.19.0

--- Formula/m/moor.rb
+++ Formula/m/moor.rb
@@ -1,8 +1,8 @@
 class Moor < Formula
   desc "Nice to use pager for humans"
   homepage "https://github.com/walles/moor"
-  url "https://github.com/walles/moor/archive/refs/tags/v2.18.0.tar.gz"
-  sha256 "b6b0fddc4d3eb622af3181fa52343705387d017d59114d564cd540a11a0f3f0e"
+  url "https://github.com/walles/moor/archive/refs/tags/v2.19.0.tar.gz"
+  sha256 "47a5da205f0af3711a164f579dfebc7c765b54cd9360a353df477c7370b88699"
   license "BSD-2-Clause"
   head "https://github.com/walles/moor.git", branch: "master"
 
