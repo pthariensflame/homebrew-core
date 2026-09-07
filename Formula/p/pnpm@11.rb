21de62b08b7f0a2d14cb52a44f00780d77bea7e3
botantony <antonsm21@gmail.com>
2026-09-07 16:55:56 +0200

    pnpm@11 11.26.0 (new formula)
    Signed-off-by: botantony <antonsm21@gmail.com>

---
+++ Formula/p/pnpm@11.rb
@@ -0,0 +1,50 @@
+class PnpmAT11 < Formula
+  desc "Fast, disk space efficient package manager"
+  homepage "https://pnpm.io/"
+  url "https://registry.npmjs.org/pnpm/-/pnpm-11.26.0.tgz"
+  sha256 "c332207e738f84b7ccf95d5f21f6fdf10b0dc5b133f0a349834a26fd17abacd8"
+  license "MIT"
+  compatibility_version 1
+
+  livecheck do
+    url "https://registry.npmjs.org/pnpm/latest-11"
+    strategy :json do |json|
+      json["version"]
+    end
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "node" => [:build, :test]
+
+  # downloads npm packages during install
+  allow_network_access! :build
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+    bin.install_symlink bin/"pnpm" => "pnpm@11"
+    bin.install_symlink bin/"pnpx" => "pnpx@11"
+
+    generate_completions_from_executable(bin/"pnpm", "completion")
+
+    # remove non-native architecture pre-built binaries
+    (libexec/"lib/node_modules/pnpm/dist").glob("**/reflink.*.node").each do |f|
+      next if f.arch == Hardware::CPU.arch
+
+      rm f
+    end
+  end
+
+  def caveats
+    <<~EOS
+      pnpm requires a Node installation to function. You can install one with:
+        brew install node
+    EOS
+  end
+
+  test do
+    system bin/"pnpm", "init"
+    assert_path_exists testpath/"package.json", "package.json must exist"
+  end
+end
