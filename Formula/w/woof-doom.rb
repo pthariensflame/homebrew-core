697d2dd58e660023db653f2cb66273705b82c333
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-08 09:29:34 +0000

    woof-doom 16.0.0

--- Formula/w/woof-doom.rb
+++ Formula/w/woof-doom.rb
@@ -1,6 +1,8 @@
 class WoofDoom < Formula
   desc "Woof! is a continuation of the Boom/MBF bloodline of Doom source ports"
   homepage "https://fabiangreffrath.github.io/woof/"
+  url "https://github.com/fabiangreffrath/woof/archive/refs/tags/woof_16.0.0.tar.gz"
+  sha256 "293bc5ad61eaac191ee1cb652c797819459a7f7819d4aec78a847c8e13695342"
   license all_of: [
     # Default license is GPL-2.0-or-later but `woof` binary ends up GPL-3.0-or-later
     "GPL-2.0-or-later",
@@ -15,19 +17,10 @@ class WoofDoom < Formula
     "MIT",          # src/i_flickstick.*, src/i_gyro.*, src/nano_bsp.*, base/all-all/dmxopl.op2, third-party/miniz/*
     "NCL",          # third-party/pffft/*
     :public_domain, # third-party/md5/*
-
-    # TODO: Licenses for HEAD code to uncomment in 16.0.0+
-    # "CC-BY-SA-4.0", # textscreen/fonts/hauge-8x18-v1-6.png
-    # "Zlib",         # netlib
+    "CC-BY-SA-4.0", # textscreen/fonts/hauge-8x18-v1-6.png
+    "Zlib",         # netlib
   ]
-
-  stable do
-    url "https://github.com/fabiangreffrath/woof/archive/refs/tags/woof_15.3.0.tar.gz"
-    sha256 "ace929952479bf42f2bbf404f6bc95ca5fabde23f3c8d656c6d1339b9baebbcc"
-
-    depends_on "sdl2-compat"
-    depends_on "sdl2_net"
-  end
+  head "https://github.com/fabiangreffrath/woof.git", branch: "master"
 
   bottle do
     sha256 cellar: :any,                 arm64_tahoe:   "5fe5942382f6b24dfd928f023279f36ea7fc8686a92affbfe9dac93d4df6ea50"
@@ -38,18 +31,13 @@ class WoofDoom < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "6f25b3b170b7b2b6bd627f551ea0dd4fe229898bf039a72508a441fd1ecf4ca6"
   end
 
-  head do
-    url "https://github.com/fabiangreffrath/woof.git", branch: "master"
-
-    depends_on "sdl3"
-  end
-
   depends_on "cmake" => :build
   depends_on "fluid-synth"
   depends_on "libebur128"
   depends_on "libsndfile"
   depends_on "libxmp"
   depends_on "openal-soft"
+  depends_on "sdl3"
   depends_on "yyjson"
 
   on_linux do
@@ -72,7 +60,7 @@ class WoofDoom < Formula
       Invalid IWAD file
     EOS
 
-    expected_output = "Error: Failed to load test_invalid.wad"
+    expected_output = "Failed to load test_invalid.wad"
     assert_match expected_output, shell_output("#{bin}/woof -nogui -iwad test_invalid.wad 2>&1", 255)
 
     assert_match version.to_s, shell_output("#{bin}/woof -version")
