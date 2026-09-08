457dfb5d103cb81f868cebffbba6fa872ca4dd0d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-08 17:29:03 +0000

    sandvault: update 1.31.0 bottle.

--- Formula/s/sandvault.rb
+++ Formula/s/sandvault.rb
@@ -6,7 +6,7 @@ class Sandvault < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "27fc626a829b91f6abaf3bd092e13d9603293759a094a1bbee77552053dfe8d9"
+    sha256 cellar: :any_skip_relocation, all: "bf0ee3a01005445d8a835f6bd0634117f360b1658a2ef83470e933db575ca0cf"
   end
 
   depends_on :macos
