620bb4243b3a7c0eedd01ba773075080792bfa9e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-08 08:40:28 +0000

    knot 3.6.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/k/knot.rb
+++ Formula/k/knot.rb
@@ -1,9 +1,10 @@
 class Knot < Formula
   desc "High-performance authoritative-only DNS server"
   homepage "https://www.knot-dns.cz/"
-  url "https://knot-dns.nic.cz/release/knot-3.5.8.tar.xz"
-  sha256 "4197c902feccf32475b254c7ddaa1ac123700e3895f50b80103ffeb8352a2807"
+  url "https://knot-dns.nic.cz/release/knot-3.6.0.tar.xz"
+  sha256 "922894f04a2835131a24c3b3edcbf761273c1b37d3dc4e46d6923ee3856af130"
   license all_of: ["GPL-3.0-or-later", "0BSD", "BSD-3-Clause", "LGPL-2.0-or-later", "MIT"]
+  compatibility_version 1
 
   livecheck do
     url "https://www.knot-dns.cz/download/"
@@ -37,6 +38,8 @@ class Knot < Formula
   depends_on "userspace-rcu"
 
   uses_from_macos "libedit"
+
+  deny_network_access! :test
 
   def install
     system "autoreconf", "--force", "--install", "--verbose" if build.head?
@@ -88,8 +91,12 @@ class Knot < Formula
   end
 
   test do
-    system bin/"kdig", "@94.140.14.140", "www.knot-dns.cz", "+quic"
-    system bin/"khost", "brew.sh"
+    (testpath/"example.zone").write <<~EOS
+      example.test. 3600 IN SOA ns.example.test. hostmaster.example.test. 1 3600 600 86400 3600
+      example.test. 3600 IN NS ns.example.test.
+      ns.example.test. 3600 IN A 127.0.0.1
+    EOS
+    system bin/"kzonecheck", "-o", "example.test.", testpath/"example.zone"
     system sbin/"knotc", "conf-check"
   end
 end
