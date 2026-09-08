e31f2f6f67cbe72ddf18ef6af14f481ca898aea6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-08 18:09:50 +0000

    todoist-cli 5.2.2

--- Formula/t/todoist-cli.rb
+++ Formula/t/todoist-cli.rb
@@ -1,8 +1,8 @@
 class TodoistCli < Formula
   desc "Official command-line interface for Todoist"
   homepage "https://github.com/Doist/todoist-cli"
-  url "https://registry.npmjs.org/@doist/todoist-cli/-/todoist-cli-5.2.1.tgz"
-  sha256 "e88c79ae29cf0bd5e5ee8fd949cf3bf2152104dca1c308218a3afb5e92578ad9"
+  url "https://registry.npmjs.org/@doist/todoist-cli/-/todoist-cli-5.2.2.tgz"
+  sha256 "17782209f9c61dd0e7b5c8e60d20c470715a077b405020e1a80c8546781c32a5"
   license "MIT"
 
   bottle do
@@ -13,7 +13,21 @@ class TodoistCli < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8014e336c0b32952e244f10e2899b2bb596adf64bec767f24516471b595ed39"
   end
 
+  depends_on "rust" => :build
   depends_on "node"
+
+  resource "keyring" do
+    url "https://github.com/Brooooooklyn/keyring-node/archive/refs/tags/v2.0.0.tar.gz"
+    sha256 "0a3eb14fe07b733e945d25d1a5425021c728ed19886f426d22afa84fc97c7754"
+
+    livecheck do
+      url "https://raw.githubusercontent.com/Doist/todoist-cli/v#{LATEST_VERSION}/package-lock.json"
+      regex(/^v?(\d+(?:\.\d+)+)$/i)
+      strategy :json do |json, regex|
+        json.dig("packages", "node_modules/@napi-rs/keyring", "version")&.[](regex, 1)
+      end
+    end
+  end
 
   def install
     system "npm", "install", *std_npm_args
@@ -21,7 +35,17 @@ class TodoistCli < Formula
 
     return unless OS.mac?
 
-    deuniversalize_machos libexec/"lib/node_modules/@doist/todoist-cli/node_modules/app-path/main"
+    node_modules = libexec/"lib/node_modules/@doist/todoist-cli/node_modules"
+
+    resource("keyring").stage do
+      system "cargo", "build", "--lib", "--release"
+      dylib = Pathname.pwd/"target/release/libnapi_keyring.dylib"
+      node_modules.glob("@doist/cli-core/node_modules/@napi-rs/keyring-darwin-*/*.node").each do |prebuilt|
+        cp dylib, prebuilt
+      end
+    end
+
+    deuniversalize_machos node_modules/"app-path/main"
   end
 
   def caveats
