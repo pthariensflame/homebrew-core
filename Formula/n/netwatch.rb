929dfaaabfef1efbf2dc27b10c12a756fa32ac1f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-08 00:21:26 +0000

    netwatch: update 0.30.2 bottle.

--- Formula/n/netwatch.rb
+++ Formula/n/netwatch.rb
@@ -6,11 +6,11 @@ class Netwatch < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c8f54fdc161227970b4ac55779f835c18393ec0ef24c17616cf73986227a048c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "76763270627f6d5ec4cdccdb3a9c254a914fde5ef5af0087139b1524a5bbfc6a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "98fc120b48dc7191960016317bd69cc53b8ba5c24d7c05c29c2cbcc86b58bee8"
-    sha256 cellar: :any,                 arm64_linux:   "cfc589ad309b83d6e994926d0a300e792ab9326411c8d8d182ce15b32dbae494"
-    sha256 cellar: :any,                 x86_64_linux:  "72feb6d365fd13f4b9573abf2d921bf673763378cb6318c2b926afd3dfb13b60"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c492ffb5700ab8d332db554456d4b95aa47f75cc467c13e4408310f119f24c94"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "44e84c5bd132e593c9c75817d7377c5037dfa39729df32d984c6750089e3bb29"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3c33e48a31a575d88ca7593c51698ac2cfeed4694ea26e8b57da487467e68f9f"
+    sha256 cellar: :any,                 arm64_linux:   "1515dc224f0f14f3c5859524afeac6d2246b2baa9f24b494b9fc9a3aeb522b91"
+    sha256 cellar: :any,                 x86_64_linux:  "43a5d6c6d95af64ed60c6babea70dea22b8e04834340217883ea9d05dbdb06a6"
   end
 
   depends_on "rust" => :build
