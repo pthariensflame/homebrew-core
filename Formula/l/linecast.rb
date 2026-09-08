e84468a38b8196c28907b5ac95453c41a0c857ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-08 17:43:28 +0000

    linecast 2.3.3

--- Formula/l/linecast.rb
+++ Formula/l/linecast.rb
@@ -3,8 +3,8 @@ class Linecast < Formula
 
   desc "Weather, tides, the sun, the moon, and maps, drawn for the terminal"
   homepage "https://github.com/ashuttl/linecast"
-  url "https://files.pythonhosted.org/packages/f6/be/02e33ef96d7822b40b1996f6105b8269027096b6c2af4abeacf0c9b59093/linecast-2.3.2.tar.gz"
-  sha256 "84cbe5a3bb927d9e1f6aecb8e1bca6848b403a27977a32dd8aae3aea060414c4"
+  url "https://files.pythonhosted.org/packages/fb/76/7a15f60a89357db13211d593abee925470b07a93ba692b18783d8182dc70/linecast-2.3.3.tar.gz"
+  sha256 "29edaed3151969b4055fd52b3fe65e4ca552c6df875f9cc782d9c720ad002c66"
   license "MIT"
 
   bottle do
