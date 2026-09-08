3a1f0f7177e86d4920f2b3886d52e5f7bcc42ab7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-08 09:54:40 +0000

    trealla-prolog: update 3.9.49 bottle.

--- Formula/t/trealla-prolog.rb
+++ Formula/t/trealla-prolog.rb
@@ -7,11 +7,11 @@ class TreallaProlog < Formula
   head "https://github.com/trealla-prolog/trealla-prolog.git", branch: "main"
 
   bottle do
-    sha256 arm64_tahoe:   "259dc10f754e59068934e9d271da46997330ac7ef7e3e28594be296dd5ef6b6a"
-    sha256 arm64_sequoia: "aeba1bf71dffc37074e8a16c1412d2787ae8de61adb4fa8503952b709d4f7e2d"
-    sha256 arm64_sonoma:  "508ac8427973f835780a3cb512bac3d71b1829049798b8b4f0fa352a6a7424bd"
-    sha256 arm64_linux:   "2564c13f03632169a3febde608110bdcf92d365f3c1510a7010a434d26eab141"
-    sha256 x86_64_linux:  "9ec095826ce45212c278a236956fa6fe12218ae42da5b613869606a9a5646545"
+    sha256 arm64_tahoe:   "70b051b90c821e57048e55f11538d0533c74b914328f10a383dc88d3df6f565e"
+    sha256 arm64_sequoia: "b0e8ddea3580eea7d65b909b6acd80a98352ef4f946766b797bae7e0431e5cff"
+    sha256 arm64_sonoma:  "012b9683c68b240dc323b22d45d61666eddfb7d07054e139514bb8b3e58084cb"
+    sha256 arm64_linux:   "621cbc333968afb11bea52b86f552c52403dc92a00f8aebf89be4e6697e5b57f"
+    sha256 x86_64_linux:  "51ce94b5b04c0da0e2d46259613bec6531c66327d4cab1d723f188abf36b369f"
   end
 
   depends_on "openssl@4"
