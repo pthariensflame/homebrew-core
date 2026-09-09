9045a07c5fe6c2c6bf48ff51159d24b2dbe98e77
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 17:51:37 +0000

    node-build 5.4.53

--- Formula/n/node-build.rb
+++ Formula/n/node-build.rb
@@ -1,8 +1,8 @@
 class NodeBuild < Formula
   desc "Install NodeJS versions"
   homepage "https://github.com/nodenv/node-build"
-  url "https://github.com/nodenv/node-build/archive/refs/tags/v5.4.52.tar.gz"
-  sha256 "bf7b8605b115cf485928ae56fd2a4bd0a97f3d9b20346d0a78243fa3df15da1d"
+  url "https://github.com/nodenv/node-build/archive/refs/tags/v5.4.53.tar.gz"
+  sha256 "1ae8090ca9db0c5ffbba0aeb2835caf90f5312511cbe2542e5a304864d161657"
   license "MIT"
   compatibility_version 1
   head "https://github.com/nodenv/node-build.git", branch: "main"
