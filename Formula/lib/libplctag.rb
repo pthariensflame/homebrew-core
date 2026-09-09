9f0b0142961e88eabc9624ba06b1a908156d67ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 11:21:18 +0000

    libplctag 2.7.2

--- Formula/lib/libplctag.rb
+++ Formula/lib/libplctag.rb
@@ -1,8 +1,8 @@
 class Libplctag < Formula
   desc "Portable and simple API for accessing AB PLC data over Ethernet"
   homepage "https://github.com/libplctag/libplctag"
-  url "https://github.com/libplctag/libplctag/archive/refs/tags/v2.7.1.tar.gz"
-  sha256 "963dba33c6957586a30d40360f00e650b1842361c7838e16e5e8dc29feeb2cb7"
+  url "https://github.com/libplctag/libplctag/archive/refs/tags/v2.7.2.tar.gz"
+  sha256 "09057d893a418e10c977267fe57a0195a4b8a4d7e512acc2d9ccef0314823056"
   license any_of: ["LGPL-2.0-or-later", "MPL-2.0"]
 
   livecheck do
