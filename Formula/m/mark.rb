f1e3ab5ab74c7519c6b730de891452ea2b66d5cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 22:23:58 +0000

    mark: update 16.19.0 bottle.

--- Formula/m/mark.rb
+++ Formula/m/mark.rb
@@ -7,11 +7,11 @@ class Mark < Formula
   head "https://github.com/kovetskiy/mark.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "555d471996d65690148cb893af33c7e11ed8c018d7c9cb9fdb1d147e221e5dca"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "555d471996d65690148cb893af33c7e11ed8c018d7c9cb9fdb1d147e221e5dca"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "555d471996d65690148cb893af33c7e11ed8c018d7c9cb9fdb1d147e221e5dca"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f446729754dff90c96250ee8936e59bee27a3e20438760c0816ab3880095f884"
-    sha256 cellar: :any,                 x86_64_linux:  "dde1ee0c7786458ba4fda093ba610cac68f5a27a12603a157c3e384baaaf1fcd"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2ca320e473618f5de7aa0be3d671e77241d4fb4d02f39a6d51f7972fdce97e26"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2ca320e473618f5de7aa0be3d671e77241d4fb4d02f39a6d51f7972fdce97e26"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2ca320e473618f5de7aa0be3d671e77241d4fb4d02f39a6d51f7972fdce97e26"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a8641b51475e7bf45d916cddcf79c4a03a810a6ced21659e718a16039845f38f"
+    sha256 cellar: :any,                 x86_64_linux:  "85cacfcfacbda3833c1d91ea29c3bca86019e62072e98b93c20213733d101811"
   end
 
   depends_on "go" => :build
