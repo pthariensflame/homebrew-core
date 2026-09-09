9aff7856f560d08d574516a9f482a9b80c85f47b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 16:46:33 +0000

    scilla: update 1.3.4 bottle.

--- Formula/s/scilla.rb
+++ Formula/s/scilla.rb
@@ -7,12 +7,11 @@ class Scilla < Formula
   head "https://github.com/edoardottt/scilla.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "968a6a0a37bf3b81c4fc81e7f9d0efb785804ec0985592df6539928b3cf0611e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "968a6a0a37bf3b81c4fc81e7f9d0efb785804ec0985592df6539928b3cf0611e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "968a6a0a37bf3b81c4fc81e7f9d0efb785804ec0985592df6539928b3cf0611e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "eb32cbea3e5b752249679b19271065d1f162db6e10ed3d134fbbf9755743df2d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "cf1bbd9b3cccf8550bcb1c314a148dda1b345d2c197fb50a18cc7cee65ac2d9b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7f3039444e569c1e56d1db44299961cbe0262f39875faaea24cb804616d33f02"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0b143f09dc5934d6b3cada68a00d3c74c1698bafb1d0d8656c0968c81bec95cc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0b143f09dc5934d6b3cada68a00d3c74c1698bafb1d0d8656c0968c81bec95cc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0b143f09dc5934d6b3cada68a00d3c74c1698bafb1d0d8656c0968c81bec95cc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1ec399d2a8a7b3dac091eaf0b296de3f8d295a4743da1773e8d2bb9b750159a9"
+    sha256 cellar: :any,                 x86_64_linux:  "5f6669a27970bf70cb89a4f937a9b08ea5878ee4d8883d65e905425c5c5d2245"
   end
 
   depends_on "go" => :build
