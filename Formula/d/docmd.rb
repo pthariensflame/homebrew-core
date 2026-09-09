cde79b39ec82d37e511f840a225eafe0c5959826
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 17:49:36 +0000

    docmd: update 0.9.5 bottle.

--- Formula/d/docmd.rb
+++ Formula/d/docmd.rb
@@ -6,7 +6,7 @@ class Docmd < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "b27209ffab4d8e7a528f8df90d6a6e22f24ad2bcd27ee2bf4e448b4264251126"
+    sha256 cellar: :any_skip_relocation, all: "93dc4632cdd5a919c0c1cff0ce02305c7d207386239dbff9a4e9b07d3ddf0748"
   end
 
   depends_on "esbuild" # for prebuilt binaries
