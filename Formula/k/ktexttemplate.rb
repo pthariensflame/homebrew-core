e55608aa82afb7f18bfe6061899a276c5c8d2da8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 17:12:21 +0000

    ktexttemplate 6.30.0

--- Formula/k/ktexttemplate.rb
+++ Formula/k/ktexttemplate.rb
@@ -1,8 +1,8 @@
 class Ktexttemplate < Formula
   desc "Libraries for text templating with Qt"
   homepage "https://api.kde.org/ktexttemplate-index.html"
-  url "https://download.kde.org/stable/frameworks/6.29/ktexttemplate-6.29.0.tar.xz"
-  sha256 "6ca3b4be8a76fff109297a5c6b1d23f5d55fa4b4110575d4c3287ee8b10619a9"
+  url "https://download.kde.org/stable/frameworks/6.30/ktexttemplate-6.30.0.tar.xz"
+  sha256 "c3c229944d25294102e4e8a5b49fa0c0f481da9d33f8bec3782e8a53afd47493"
   license "LGPL-2.1-or-later"
   head "https://invent.kde.org/frameworks/ktexttemplate.git", branch: "master"
 
