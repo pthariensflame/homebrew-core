8235fb5342e4734bb8b967a8cb428db8ab411abe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 11:56:01 +0000

    werf 2.76.0

--- Formula/w/werf.rb
+++ Formula/w/werf.rb
@@ -1,8 +1,8 @@
 class Werf < Formula
   desc "Consistent delivery tool for Kubernetes"
   homepage "https://werf.io/"
-  url "https://github.com/werf/werf/archive/refs/tags/v2.75.3.tar.gz"
-  sha256 "44efae6e7542a7d929fcc222000f633372239ff24e3d079beacd980adb461070"
+  url "https://github.com/werf/werf/archive/refs/tags/v2.76.0.tar.gz"
+  sha256 "414cd8dde95536f0a96f783f7968c46d5ecb4e4bfee0157fefa7cfb449879ce0"
   license "Apache-2.0"
   head "https://github.com/werf/werf.git", branch: "main"
 
