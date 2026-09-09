2c5744b756e152120181503715620cf03eea35ba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 02:58:18 +0000

    licenseplist: update 3.28.1 bottle.

--- Formula/l/licenseplist.rb
+++ Formula/l/licenseplist.rb
@@ -6,10 +6,9 @@ class Licenseplist < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "11edfae88a7fa250f2b2d2487dbc40e1b263c4a273c54a3d60a7bfdb058cab60"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0c2701c708e8414227d67577475c92befbc5e3cca8b732c04031168af79c0ca8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "158ab1e35ea5e04c71c1a481c26b23a789ca58ed4500cda04c8fc17851be4be3"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e76717a115d53914e6782aac2ab36812c2d0a43ab182cc4e0f9d51fee6e04016"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4ebbf9914a8d3bbb7e26402146e8bdd40afdb06e96c4fcb5e2021aa809468819"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "484d121cf1dc55edb596f629f8c4106643ef57d44936ea6c5db31ff73214707d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a5fc39108fe8afb861e52610df295ed5328443802f9b1e6376f0987385825007"
   end
 
   depends_on :macos
