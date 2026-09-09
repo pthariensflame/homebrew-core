cb3b604870e313430a5543a2cf5c8ba93e04f25e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 14:47:52 +0000

    lfk: update 0.18.10 bottle.

--- Formula/l/lfk.rb
+++ Formula/l/lfk.rb
@@ -6,11 +6,11 @@ class Lfk < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "89313c59e70d0f128e493c32462de29a4c5f801bc8dff2166805e4b12c82f4f6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6983512546496f384bb82b0d5327eaa329f6e9acf81324e7c36b89af3ba9867e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9906cc1f1373c58135e4985ebf3fe1aee646da51f2dbd765b3748b2062a86c13"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ea05ed6f7ff3ea3b0587cbcfd5f113f977bf9b9c858c321b99fc58d652fff8ba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "05c1993623ecb4a2c236d19fc03164600e5fe5643879b53836e7d41a16f3598c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "efaa5deb4340e89915ed71734e76e536868edf30f642b22dd90eee43d237cfea"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "511dc2376592fbf4215dbfad1d28bf36d75247661375e1de8c315507e717f8d7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "133fd9898732c750d08438e43130eb61ea82403e44acc54a94c591961c4a2691"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "cff702b4a4cfb50236a3337e6721af11a50a4dd9f9e74b7c0fc22f037d70b63f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "404f9f09285d4569ae0672e67fb082bf0f67082169a4c3cfe37468c0cfbf266c"
   end
 
   depends_on "go" => :build
