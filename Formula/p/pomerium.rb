0ddae52140be2c65a128e024de97b37f1d6330ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-09 18:00:13 +0000

    pomerium 0.33.3

--- Formula/p/pomerium.rb
+++ Formula/p/pomerium.rb
@@ -1,8 +1,8 @@
 class Pomerium < Formula
   desc "Identity and context-aware access proxy"
   homepage "https://www.pomerium.com"
-  url "https://github.com/pomerium/pomerium/archive/refs/tags/v0.33.1.tar.gz"
-  sha256 "fefa6c314d72f8baa17d1736a53840b39fddefe57c3a22ca6bf71a0d3d7091df"
+  url "https://github.com/pomerium/pomerium/archive/refs/tags/v0.33.3.tar.gz"
+  sha256 "4ff8ca584b0350ba6cac14cec692515fac76510d758419c51c5d7f4bd88381bd"
   license "Apache-2.0"
 
   head "https://github.com/pomerium/pomerium.git", branch: "main"
