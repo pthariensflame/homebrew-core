ffa901dfd1f221625561471fcbb5ce9f4e5ef5dd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-10 15:08:40 +0000

    dtop: update 0.9.3 bottle.

--- Formula/d/dtop.rb
+++ Formula/d/dtop.rb
@@ -7,11 +7,11 @@ class Dtop < Formula
   head "https://github.com/amir20/dtop.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "577ae9f73a96ae23bf75d9a02ccc1c9f77109b851ed43095544bf98ece077878"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6f36f664ce3c6a541b37639093258e89ff73c22dfddeacf7359e333292ee0dbe"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d10df74dac3603b3ebea2f06529b9c581c258f0dd9b80a3992ed7fb35de2047b"
-    sha256 cellar: :any,                 arm64_linux:   "7fb7cb7acfba846a5a93e64c5f43249c76be18fad099de63578d31db45f8ca53"
-    sha256 cellar: :any,                 x86_64_linux:  "025bca9fc7b7fc41f48c691c1a7575d48dfc01885a487cba0e823e385db78643"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2cdaec585aec9ee28c9c3917f3a3e6cdc3bf90ad32e20c2f822f3e18c90dc1dc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6d94d8c30a64540c8ae34b1a99006b680efce37e1555ed7975309875eabccffb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1b64df51b15ded36bdc795d02f6f5267168af26962334ec70006416bdfd91950"
+    sha256 cellar: :any,                 arm64_linux:   "4e75a2f154853cc72adfe15444291cfa7c7b28f6a43ebf871ddf29a9fa8478eb"
+    sha256 cellar: :any,                 x86_64_linux:  "fd498cab959ce4bf1b66ed7370ad86e3b7dc5cc4fa7b68504342355726e750ee"
   end
 
   depends_on "rust" => :build
