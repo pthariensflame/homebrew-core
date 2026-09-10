83947275cd7e954367157dba39b26bdb82bc06e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-10 21:12:42 +0000

    cfn-lint: update 1.56.2 bottle.

--- Formula/c/cfn-lint.rb
+++ Formula/c/cfn-lint.rb
@@ -8,11 +8,11 @@ class CfnLint < Formula
   license "MIT-0"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "44053f0d803be4a6b22522de8c4f99d753448bb3ee3e87ef39b41f0b9459a635"
-    sha256 cellar: :any, arm64_sequoia: "4e78d6090001c3577ce418fd846a5871375c94c826215331be7dc78fbd6b1a5d"
-    sha256 cellar: :any, arm64_sonoma:  "27c919f98e025d23bcc86284f650bc787494152a034f1343745cdfe170fba942"
-    sha256 cellar: :any, arm64_linux:   "701ca97c8801217f92e940ffb393a03f16306cb33657615173c4b77d9996677e"
-    sha256 cellar: :any, x86_64_linux:  "a7e98c90b1a368e4442885be279d2038a6a0dd15fc8083475aecef5ac10484ee"
+    sha256 cellar: :any, arm64_tahoe:   "cd61a957adb203b3df686aa566dc1469094a2380cbc9c5f43663fb858941f505"
+    sha256 cellar: :any, arm64_sequoia: "0995a9d934d73048fc10ad12a36a49dc1fef36595935e8e94d7db89e7ef29f23"
+    sha256 cellar: :any, arm64_sonoma:  "b179ce66bc7820b5b1fcafcc514e26adba4f63f178a86ac451334b83ea9615d2"
+    sha256 cellar: :any, arm64_linux:   "c48efddf81127f34c0f8e1887d86eca887c43e005e629e2a3bd4e1c2ba2af924"
+    sha256 cellar: :any, x86_64_linux:  "c7053d50b5c7eff0b5fb3e3bd710fe06a19e2f34d6fe050f8904e71d942edd5f"
   end
 
   depends_on "libyaml"
