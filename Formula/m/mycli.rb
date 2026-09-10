32f4ca0d93c3ff87f152f10592c6b601ac62cfa3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-10 22:33:23 +0000

    mycli: update 2.23.0 bottle.

--- Formula/m/mycli.rb
+++ Formula/m/mycli.rb
@@ -8,11 +8,11 @@ class Mycli < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "7344e462567d1969cb0b95c124318c3b9b51416cae02f651cdccaeceb21c87f3"
-    sha256 cellar: :any, arm64_sequoia: "90eba455eb6d54601f551fca99ddda3a3ef0ce74f7869cab2ad2bba5e66a02f0"
-    sha256 cellar: :any, arm64_sonoma:  "0df1167956a4d591b895e4905fc43c62f64e62f5bae9d010bb61bb8e374bb4a6"
-    sha256 cellar: :any, arm64_linux:   "91d89a5dd675d94f2f8a7004c6fcdfa84dfffaa9aaf933a3f9db3b035332e926"
-    sha256 cellar: :any, x86_64_linux:  "60e9d960b2d1aaf29b69f26595a7aab4dee970655bfada1b134fcf03668bd47c"
+    sha256 cellar: :any, arm64_tahoe:   "4208afd5047aef177e9e1dc217ea885a4f93ca656a38979c6ec6a03f73a282c4"
+    sha256 cellar: :any, arm64_sequoia: "153e479c047f8ca5790098842cc08263d7422c561bcf21ebe460627ce81f77f0"
+    sha256 cellar: :any, arm64_sonoma:  "f3cbf2c7208baacf0effaefdc250c5ce5bdab105c8381ca6f5caede5be3de51d"
+    sha256 cellar: :any, arm64_linux:   "991d09063339bc18c22375c468182d71f4e5dbd16e7c6e23a02596b55bc31059"
+    sha256 cellar: :any, x86_64_linux:  "824cb24adbaa7c0a6a2863037e49743c2e618a9fba9258977ed09386c9e10ee7"
   end
 
   depends_on "rust" => :build # for jiter, polars, vl-convert
