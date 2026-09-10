9a1dc7e6a0915f44e8267b311d7e6262b392eca5
Ruoyu Zhong <zhongruoyu@outlook.com>
2026-09-10 13:38:36 +0000

    mdcat: update 2.16.1 bottle.

--- Formula/m/mdcat.rb
+++ Formula/m/mdcat.rb
@@ -7,11 +7,11 @@ class Mdcat < Formula
   head "https://github.com/BIRSAx2/mdcat.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c8f8fa69ff79691cacb882d2c53650ea817349a7a5c2b356d4aef7d8bdba5c5d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bcb864e22d1e5427de1860fbd0b65b2fd1aff6135531c63a5a58fe2652104d29"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "99bd2f54d8acfa6f139b019d73319643dbf9a0fcc541483c21dfee02623d1f47"
-    sha256 cellar: :any,                 arm64_linux:   "2e579ae6eab7545c4150745ae6634aa815a377813d216a1e94b99ca4f4aa7221"
-    sha256 cellar: :any,                 x86_64_linux:  "8c5792d025c7874c51ae8baa5da3c58ebe8018383e4b2c58f215159206ad97a1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6337a9b0c6d8e0eeb7989bbd90ae3b63669da509a5811929c8db19680921ca97"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4969bf7c74b5d1482447e188382cb89fceda15f6e6115244f724e3e6f5f417a5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "80680d5a2ff34a7a735eb2d6cf0b8ba28dd5674e38b1280a153628ff93da3eea"
+    sha256 cellar: :any,                 arm64_linux:   "9df1d61a0cb044c9da90a2a5be7b3145bd7c2a11483353b338c374dabe614d14"
+    sha256 cellar: :any,                 x86_64_linux:  "ff32732cbdc6971cb77fa47492e6f561fb2e6badcc04980c9e542e9785727336"
   end
 
   depends_on "asciidoctor" => :build
