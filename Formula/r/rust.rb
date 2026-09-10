6bde15d4022060f35578b270c22dfb3257f9602b
Ruoyu Zhong <zhongruoyu@outlook.com>
2026-09-10 01:38:56 +0000

    rust: update 1.98.1 bottle.

--- Formula/r/rust.rb
+++ Formula/r/rust.rb
@@ -87,11 +87,12 @@ class Rust < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "6d42a8255e121710825d43a83cdaf28fb9d2be8d25159d2170805efe5f8493ae"
-    sha256 cellar: :any, arm64_sequoia: "449dff6487939ed5d3b623c675ed8ca4e015ad4bc3b6dd53c4d063a924866e7f"
-    sha256 cellar: :any, arm64_sonoma:  "6bddeec978894846e6d92b0ae6718137c4d43d999fd15f36cc0caa71e34d75a5"
-    sha256 cellar: :any, arm64_linux:   "3c60ded411b279386f8fe955fbdfa668f30bde1f89ede40d11dfa2ab611dfb41"
-    sha256 cellar: :any, x86_64_linux:  "7d925d70482067a119020630634f7e12cde7c738a7066d402729fb1d59051d4d"
+    rebuild 1
+    sha256 cellar: :any, arm64_tahoe:   "409411d3aacd29a2528ca290f5fbbaca4acb2f1db8fa44c3d30480c30e79231b"
+    sha256 cellar: :any, arm64_sequoia: "4a1bbbb08569ac2f1837bed5ce931e707f6c4666118d933f6372cb9438d7794a"
+    sha256 cellar: :any, arm64_sonoma:  "5928033c1a2b6639206995f9adf81d82d1d29fa4c7364628fb3e68b031b6d61e"
+    sha256 cellar: :any, arm64_linux:   "3f53535b1c1bc548a7282e6c23beaca99def881a5786354aba04d0cb3f572166"
+    sha256 cellar: :any, x86_64_linux:  "b42a09b9ba34ccbce8b84e0e8bce44680360c1ff261a3bd952fc586e6c7bd04c"
   end
 
   depends_on "libgit2"
