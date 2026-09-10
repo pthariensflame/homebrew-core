8792e1b2746ab23e9fcb735c5cb431f9af60ecd3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-10 11:52:01 +0000

    snx-rs 6.3.0

--- Formula/s/snx-rs.rb
+++ Formula/s/snx-rs.rb
@@ -1,8 +1,8 @@
 class SnxRs < Formula
   desc "Open-source client for Check Point VPN tunnels"
   homepage "https://github.com/ancwrd1/snx-rs"
-  url "https://github.com/ancwrd1/snx-rs/archive/refs/tags/v6.2.4.tar.gz"
-  sha256 "37f367ef8798dc810db57729acf40bd7696cb74144311a190070e159f4de6d7d"
+  url "https://github.com/ancwrd1/snx-rs/archive/refs/tags/v6.3.0.tar.gz"
+  sha256 "55ec490ea7203497d8b771a5c5f224b037a1872693b14f56a885dacc5d30630e"
   license "AGPL-3.0-only"
   head "https://github.com/ancwrd1/snx-rs.git", branch: "main"
 
