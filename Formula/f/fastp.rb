b54f6c45da1eacb982e1609d6b107935692744aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-10 05:00:58 +0000

    fastp 1.3.7

--- Formula/f/fastp.rb
+++ Formula/f/fastp.rb
@@ -1,8 +1,8 @@
 class Fastp < Formula
   desc "Ultra-fast all-in-one FASTQ preprocessor"
   homepage "https://github.com/OpenGene/fastp"
-  url "https://github.com/OpenGene/fastp/archive/refs/tags/v1.3.6.tar.gz"
-  sha256 "4a282e9bbcbce849cd4ed3527e59469ad2cf7cf8d3cbe99debabaca985030946"
+  url "https://github.com/OpenGene/fastp/archive/refs/tags/v1.3.7.tar.gz"
+  sha256 "5b7d6880c66e9e10e5923c68ee0c0b5a30f59bd252d836c379c74f8533c26993"
   license "MIT"
   head "https://github.com/OpenGene/fastp.git", branch: "master"
 
